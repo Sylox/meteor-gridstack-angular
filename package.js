d29a45905236cbe89fd5350141ba41455d3b51c7
André Kremser <croonix@Andres-Mac-mini.local>
2016-02-21 15:36:38 -0800

    init package

--- package.js
+++ package.js
@@ -1,24 +1,30 @@
 Package.describe({
-  name: 'sylox:meteor-gridstack-angular',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'sylox:meteor-gridstack-angular',
+    version: '0.2.4',
+    // Brief, one-line summary of the package.
+    summary: 'gridstack-angular is an AngularJS directive for the widget layout plugin gridstack.js',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('meteor-gridstack-angular.js');
-});
+    api.versionsFrom('1.2.1');
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('sylox:meteor-gridstack-angular');
-  api.addFiles('meteor-gridstack-angular-tests.js');
+    api.use([
+        'jquery',
+        'erasaur:meteor-lodash',
+        'mizzao:jquery-ui',
+        'dsyko:jquery-ui-touch-punch'
+    ], 'client');
+
+    api.addFiles([
+        'lib/gridstack/dist/gridstack.min.css',
+        'lib/gridstack/dist/gridstack-extra.min.css',
+        'lib/gridstack/dist/gridstack.min.js',
+        'lib/gridstack-angular/dist/gridstack-angular.min.js'
+    ], 'client');
+
 });
